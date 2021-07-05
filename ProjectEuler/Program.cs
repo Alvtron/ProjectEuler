8668131b3cdf1c106345de722b11bae22ab3f5b8
Thomas Angeland <thomas.angeland@gmail.com>
2021-07-05 18:38:17 +0200

    Solved problem 23.

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -9,7 +9,7 @@ namespace ProjectEuler
     {
         public static void Main(string[] args)
         {
-            Solve(0020);
+            Solve(0023);
         }
 
         private static void Solve(params int[] numbers)
