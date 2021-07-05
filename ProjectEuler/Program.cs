3b0e2f752e485438140dc2cc68e2819abca56dd0
Thomas Angeland <thomas.angeland@gmail.com>
2021-07-05 17:05:35 +0200

    Solved problem 10.

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -9,7 +9,7 @@ namespace ProjectEuler
     {
         public static void Main(string[] args)
         {
-            Solve(0009);
+            Solve(0010);
         }
 
         private static void Solve(params int[] numbers)
---
+++ ProjectEuler/Solvers/Solver_0010.cs
@@ -0,0 +1,31 @@
+﻿using ProjectEuler.Library;
+using ProjectEuler.Utilities;
+
+namespace ProjectEuler.Solvers
+{
+    public class Solver_0010 : ISolver
+    {
+        public Answer Solve()
+        {
+            return SumOfPrimesBelow(2_000_000);
+        }
+
+        private static long SumOfPrimesBelow(long limit)
+        {
+            var primeGenerator = new PrimeGenerator();
+            var sum = 0L;
+
+            foreach (var prime in primeGenerator.GetPrimes())
+            {
+                if (prime >= limit)
+                {
+                    break;
+                }
+
+                sum += prime;
+            }
+
+            return sum;
+        }
+    }
+}
