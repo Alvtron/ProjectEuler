b57b9b3826951f509b32cfd8eac08a6ada5619a1
Thomas Angeland <thomas.angeland@gmail.com>
2021-07-08 13:43:21 +0200

    Added Integer and BigInteger exensions (with unit tests).

---
+++ ProjectEuler.Tests/Extensions/IntegerExtensionsTests.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using ProjectEuler.Extensions;
+
+namespace ProjectEuler.Tests.Extensions
+{
+    public class IntegerExtensionsTests
+    {
+        [Test]
+        public void Length_PositiveNumber_ReturnsCorrectLength()
+        {
+            // ARRANGE
+            const int number = 1234;
+            const int actualLength = 4;
+
+            // ACT
+            var length = number.Length();
+
+            // ASSERT
+            Assert.That(length, Is.EqualTo(actualLength));
+        }
+
+        [Test]
+        public void Length_NegativeNumber_ReturnsCorrectLength()
+        {
+            // ARRANGE
+            const int number = -12345;
+            const int actualLength = 5;
+
+            // ACT
+            var length = number.Length();
+
+            // ASSERT
+            Assert.That(length, Is.EqualTo(actualLength));
+        }
+
+        [Test]
+        public void FirstDigits_PositiveNumber_ReturnsCorrectLength()
+        {
+            // ARRANGE
+            const int number = 123456789;
+            const int numberOfDigits = 3;
+            const int actualDigits = 123;
+
+            // ACT
+            var digits = number.FirstDigits(numberOfDigits);
+
+            // ASSERT
+            Assert.That(digits, Is.EqualTo(actualDigits));
+        }
+
+        [Test]
+        public void FirstDigits_NegativeNumber_ReturnsCorrectLength()
+        {
+            // ARRANGE
+            const int number = -123456789;
+            const int numberOfDigits = 4;
+            const int actualDigits = -1234;
+
+            // ACT
+            var digits = number.FirstDigits(numberOfDigits);
+
+            // ASSERT
+            Assert.That(digits, Is.EqualTo(actualDigits));
+        }
+
+        [Test]
+        public void LastDigits_PositiveNumber_ReturnsCorrectLength()
+        {
+            // ARRANGE
+            const int number = 123456789;
+            const int numberOfDigits = 5;
+            const int actualDigits = 56789;
+
+            // ACT
+            var digits = number.LastDigits(numberOfDigits);
+
+            // ASSERT
+            Assert.That(digits, Is.EqualTo(actualDigits));
+        }
+
+        [Test]
+        public void LastDigits_NegativeNumber_ReturnsCorrectLength()
+        {
+            // ARRANGE
+            const int number = -123456789;
+            const int numberOfDigits = 6;
+            const int actualDigits = -456789;
+
+            // ACT
+            var digits = number.LastDigits(numberOfDigits);
+
+            // ASSERT
+            Assert.That(digits, Is.EqualTo(actualDigits));
+        }
+    }
+}
---
+++ ProjectEuler/Extensions/IntegerExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ProjectEuler.Extensions
+{
+    public static class IntegerExtensions
+    {
+        public static int Length(this int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return (int)Math.Ceiling(Math.Log10(Math.Abs(number)));
+        }
+
+        public static int FirstDigits(this int number, int count)
+        {
+            if (count < 1)
+            {
+                throw new ArgumentException("The count must at least be 1.", nameof(count));
+            }
+
+            if (number == 0)
+            {
+                return number;
+            }
+
+            var sign = Math.Sign(number);
+            number = Math.Abs(number);
+
+            var numberOfDigits = number.Length();
+            
+            return numberOfDigits < count
+                ? sign * number
+                : sign * (int)Math.Truncate(number / Math.Pow(10, numberOfDigits - count));
+        }
+
+        public static int LastDigits(this int number, int count)
+        {
+            if (count < 1)
+            {
+                throw new ArgumentException("The count must at least be 1.", nameof(count));
+            }
+
+            if (number == 0)
+            {
+                return number;
+            }
+
+            var sign = Math.Sign(number);
+            number = Math.Abs(number);
+
+            return sign * number % (int)Math.Pow(10, count);
+        }
+    }
+}
