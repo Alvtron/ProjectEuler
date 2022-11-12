6f8b488ededd4b9d664ef0d137b4eb3c6f27bcb0
Thomas Angeland <thomas.angeland@gmail.com>
2022-11-12 02:38:28 +0100

    Solved problem 29.

---
+++ ProjectEuler/Solvers/Solver_0029.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProjectEuler.Library;
+
+namespace ProjectEuler.Solvers;
+
+public class Solver_0029 : ISolver
+{
+    public Answer Solve()
+    {
+        return GeneratePowerCombinations(2, 100, 2, 100)
+            .Distinct()
+            .Count();
+    }
+
+    private static IEnumerable<double> GeneratePowerCombinations(int baseMin, int baseMax, int exponentMin, int exponentMax)
+    {
+        for (var a = baseMin; a <= baseMax; a++)
+        for (var b = exponentMin; b <= exponentMax; b++)
+        {
+            yield return Math.Pow(a, b);
+        }
+    }
+}
