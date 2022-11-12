41b13d27db79e54f010fdf5da26f111891fd125d
Thomas Angeland <thomas.angeland@gmail.com>
2022-11-13 00:50:05 +0100

    Added helper classes for creating combinations and permutations.

---
+++ ProjectEuler.Tests/Helpers/CombinationsTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using ProjectEuler.Helpers;
+
+namespace ProjectEuler.Tests.Helpers;
+
+[TestFixture]
+public class CombinationsTests
+{
+    [Test]
+    public void Of_Empty_ReturnsCorrectCombinations()
+    {
+        // ARRANGE
+        var nothing = Enumerable.Empty<int>();
+
+        // ACT
+        var combinations = Combinations.Of(nothing);
+
+        // ASSERT
+        Assert.That(combinations, Is.Empty);
+    }
+
+    [Test]
+    public void Of_Digits_ReturnsCorrectCombinations()
+    {
+        // ARRANGE
+        var digits = new[] { 1, 2, 3 };
+
+        // ACT
+        var combinations = Combinations.Of(digits);
+
+        // ASSERT
+        var expected = new HashSet<int[]>
+        {
+            new[] { 1, },
+            new[] { 2, },
+            new[] { 3, },
+            new[] { 1, 2 },
+            new[] { 1, 3 },
+            new[] { 2, 3 },
+            new[] { 1, 2, 3 },
+        };
+
+        foreach (var combination in combinations)
+        {
+            Assert.That(expected.Count(e => e.SequenceEqual(combination)), Is.EqualTo(1));
+        }
+    }
+}
---
+++ ProjectEuler.Tests/Helpers/PermutationsTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using ProjectEuler.Helpers;
+
+namespace ProjectEuler.Tests.Helpers;
+
+[TestFixture]
+public class PermutationsTests
+{
+    [Test]
+    public void Of_Empty_ReturnsCorrectPermutations()
+    {
+        // ARRANGE
+        var nothing = Enumerable.Empty<int>();
+
+        // ACT
+        var permutations = Combinations.Of(nothing);
+
+        // ASSERT
+        Assert.That(permutations, Is.Empty);
+    }
+
+    [Test]
+    public void Of_Digits_ReturnsCorrectPermutations()
+    {
+        // ARRANGE
+        var digits = new[] { 1, 2, 3 };
+
+        // ACT
+        var permutations = Permutations.Of(digits);
+
+        // ASSERT
+        var expected = new HashSet<int[]>
+        {
+            new[] { 1, },
+            new[] { 1, 2 },
+            new[] { 1, 3 },
+            new[] { 1, 2, 3 },
+            new[] { 1, 3, 2 },
+            new[] { 2, },
+            new[] { 2, 1 },
+            new[] { 2, 3 },
+            new[] { 2, 1, 3 },
+            new[] { 2, 3, 1 },
+            new[] { 3, },
+            new[] { 3, 1 },
+            new[] { 3, 2 },
+            new[] { 3, 1, 2 },
+            new[] { 3, 2, 1 },
+        };
+
+        foreach (var combination in permutations)
+        {
+            Assert.That(expected.Count(e => e.SequenceEqual(combination)), Is.EqualTo(1));
+        }
+    }
+}
