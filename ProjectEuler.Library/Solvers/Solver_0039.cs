687c48c16af36cf222f580bd0ba50c8043e1bfd0
Thomas Angeland <thomas.angeland@gmail.com>
2023-03-05 13:46:21 +0100

    Added solver for problem 39.

---
+++ ProjectEuler.Library/Solvers/Solver_0039.cs
@@ -0,0 +1,56 @@
+﻿using ProjectEuler.Library.Answers;
+
+namespace ProjectEuler.Library.Solvers;
+
+public class Solver_0039 : ISolver
+{
+    public async Task<Answer> SolveAsync(CancellationToken cancellationToken = default)
+    {
+        var perimeter = FindPerimeterWithMaximumRightAngleTriangles(3, 1000);
+        return await Task.FromResult(perimeter);
+    }
+
+    private static int FindPerimeterWithMaximumRightAngleTriangles(int minimumPerimeter, int maximumPerimeter)
+    {
+        if (minimumPerimeter < 3)
+        {
+            throw new ArgumentException("The minimum perimeter must be at least 3.", nameof(minimumPerimeter));
+        }
+
+        if (minimumPerimeter > maximumPerimeter)
+        {
+            throw new ArgumentException("The minimum perimeter must be lower than the maximum perimeter.", nameof(minimumPerimeter));
+        }
+
+        var maximizedPerimeter = 0;
+        var maximizedNumberOfSolutions = 0;
+
+        for (var perimeter = minimumPerimeter; perimeter <= maximumPerimeter; perimeter++)
+        {
+            var numberOfSolutions = 0;
+
+            // a, b and c represents the length of each integral side of the triangle
+            for (var c = 1; c <= perimeter - 2; c++)
+            {
+                for (var b = 1; b <= c; b++)
+                {
+                    var a = Math.Sqrt(c * c - b * b);
+                    if (a != 0 && double.IsInteger(a) && (int)a + b + c == perimeter)
+                    {
+                        numberOfSolutions++;
+                    }
+                }
+            }
+
+            if (numberOfSolutions <= maximizedNumberOfSolutions)
+            {
+                continue;
+            }
+
+            maximizedNumberOfSolutions = numberOfSolutions;
+            maximizedPerimeter = perimeter;
+        }
+
+        return maximizedPerimeter;
+    }
+}
