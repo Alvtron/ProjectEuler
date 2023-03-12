491464ce3831a6d1c8a4e8b8d125fb17d35df29c
Thomas Angeland <thomas.angeland@gmail.com>
2023-03-12 22:28:34 +0100

    Improved prime number calculations and added unit tests.

---
+++ ProjectEuler.Mathematics.Tests/Numbers/PrimeNumbersTests.cs
@@ -0,0 +1,55 @@
+﻿using ProjectEuler.Mathematics.Numbers;
+
+namespace ProjectEuler.Mathematics.Tests.Numbers;
+
+[TestFixture]
+public class PrimeNumbersTests
+{
+    [Test]
+    public async Task IsPrime_200000Primes_ReturnsTrue()
+    {
+        // ARRANGE
+        var path = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Assets\PrimeNumbersTests_200_000_primes.txt");
+
+        // ACT & ASSERT
+        await foreach (var line in File.ReadLinesAsync(path))
+        {
+            Assert.That(long.TryParse(line, out var primeNumber), Is.True);
+            Assert.That(PrimeNumbers.IsPrime(primeNumber), Is.True, $"{primeNumber} is not a prime.");
+        }
+    }
+
+    [Test]
+    public async Task Generate_200000_AllArePrimes()
+    {
+        // ARRANGE
+        var path = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Assets\PrimeNumbersTests_200_000_primes.txt");
+        using var enumerator = PrimeNumbers.Generate().Take(200_000).GetEnumerator();
+
+        // ACT & ASSERT
+        await foreach (var line in File.ReadLinesAsync(path))
+        {
+            Assert.That(enumerator.MoveNext(), Is.True);
+            Assert.That(line, Is.EqualTo(enumerator.Current.ToString()));
+        }
+    }
+
+    [Test]
+    public async Task Between_0And1000000_AllArePrimes()
+    {
+        // ARRANGE
+        var path = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Assets\PrimeNumbersTests_200_000_primes.txt");
+        var primeEnumerator = File.ReadLinesAsync(path).GetAsyncEnumerator();
+        using var enumerator = PrimeNumbers.Between(0, 200_000).GetEnumerator();
+
+        // ACT & ASSERT
+        while (enumerator.MoveNext())
+        {
+            await primeEnumerator.MoveNextAsync();
+
+            Assert.That(primeEnumerator.Current, Is.EqualTo(enumerator.Current.ToString()));
+        }
+
+        Assert.That(enumerator.MoveNext, Is.False);
+    }
+}
--- ProjectEuler.Mathematics/Numbers/PrimeNumbers.cs
+++ ProjectEuler.Mathematics/Numbers/PrimeNumbers.cs
@@ -2,9 +2,15 @@
 
 public static class PrimeNumbers
 {
+    /// <summary>
+    /// Generates prime numbers indefinitely, in ascending order.
+    /// </summary>
+    /// <remarks>Enumeration must be stopped using linq.</remarks>
     public static IEnumerable<long> Generate()
     {
-        for (var number = 1L; true; number++)
+        yield return 2;
+
+        for (var number = 3;; number += 2)
         {
             if (!IsPrime(number))
             {
@@ -15,28 +21,105 @@ public static class PrimeNumbers
         }
     }
 
+    /// <summary>
+    /// Find all prime values between a <paramref name="minimum"/> and <paramref name="maximum"/>, in ascending order.
+    /// </summary>
+    /// <param name="minimum">The minimum number that defines the lower bound of the search space.</param>
+    /// <param name="maximum">The maximum number that defines the upper bound of the search space.</param>
+    public static IEnumerable<long> Between(long minimum, long maximum)
+    {
+        if (minimum > maximum)
+        {
+            throw new ArgumentException("The minimum was greater than the maximum.", nameof(minimum));
+        }
+
+        if (minimum == maximum && IsPrime(maximum))
+        {
+            yield return maximum;
+            yield break;
+        }
+
+        if (minimum <= 2)
+        {
+            yield return 2;
+            minimum = 3;
+        }
+        else if (long.IsEvenInteger(minimum))
+        {
+            minimum++;
+        }
+
+        for (var number = minimum; number <= maximum; number += 2L)
+        {
+            if (!IsPrime(number))
+            {
+                continue;
+            }
+
+            yield return number;
+        }
+    }
+
+    /// <summary>
+    /// Find all prime values between a <paramref name="maximum"/> and <paramref name="minimum"/>, in descending order.
+    /// </summary>
+    /// <param name="minimum">The minimum number that defines the lower bound of the search space.</param>
+    /// <param name="maximum">The maximum number that defines the upper bound of the search space.</param>
+    public static IEnumerable<long> BetweenReversed(long maximum, long minimum)
+    {
+        if (maximum < minimum)
+        {
+            throw new ArgumentException("The maximum was less than the minimum.", nameof(maximum));
+        }
+
+        if (maximum == 2)
+        {
+            yield return 2;
+            yield break;
+        }
+
+        if (minimum == maximum && IsPrime(maximum))
+        {
+            yield return maximum;
+            yield break;
+        }
+
+        if (long.IsEvenInteger(maximum))
+        {
+            maximum--;
+        }
+
+        for (var number = maximum; number >= minimum; number -= 2L)
+        {
+            if (!IsPrime(number))
+            {
+                continue;
+            }
+
+            yield return number;
+        }
+    }
+
+    /// <summary>
+    /// Determines whether the specified <paramref name="number"/> is considered a prime number.
+    /// </summary>
+    /// <param name="number">The number to check.</param>
+    /// <returns><see langword="true"/> if the number is a prime number; <see langword="false"/> otherwise.</returns>
     public static bool IsPrime(long number)
     {
-        if (long.IsNegative(number))
+        if (number is 2 or 3)
+        {
+            return true;
+        }
+
+        if (number < 2 || number % 2 == 0 || number % 3 == 0)
         {
             return false;
         }
 
-        switch (number)
+        for (var i = 5; i * i <= number; i += 6)
         {
-            case 0:
-                return false;
-            case 1:
-                return false;
-            case 2:
-                return true;
-        }
-
-        var limit = (long)Math.Ceiling(Math.Sqrt(number));
-
-        for (var divisor = 2L; divisor <= limit; ++divisor)
-        {
-            if (number % divisor == 0L)
+            if (number % i == 0 || number % (i + 2) == 0)
             {
                 return false;
             }
