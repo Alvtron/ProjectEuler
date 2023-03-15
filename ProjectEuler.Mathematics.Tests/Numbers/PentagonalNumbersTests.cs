982a8906426950daeb4237fc872f706944b1db5d
Thomas Angeland <thomas.angeland@gmail.com>
2023-03-16 00:41:43 +0100

    Added PandigitalNumbers class, with tests.

---
+++ ProjectEuler.Mathematics.Tests/Numbers/PentagonalNumbersTests.cs
@@ -0,0 +1,55 @@
+﻿using ProjectEuler.Mathematics.Numbers;
+
+namespace ProjectEuler.Mathematics.Tests.Numbers;
+
+[TestFixture]
+public class PentagonalNumbersTests
+{
+    [Test]
+    public async Task Generate_FiveHundredNumbers_AllArePentagonalNumbers()
+    {
+        // ARRANGE
+        var path = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Assets\PentagonalNumbersTests_500.txt");
+
+        // ACT
+        using var pentagonalEnumerator = PentagonalNumbers.Generate(500).GetEnumerator();
+
+        // ASSERT
+        await foreach (var line in File.ReadLinesAsync(path))
+        {
+            Assert.That(long.TryParse(line, out var expectedPentagonal), Is.True);
+            Assert.That(pentagonalEnumerator.MoveNext(), Is.True);
+            Assert.That(pentagonalEnumerator.Current, Is.EqualTo(expectedPentagonal));
+        }
+    }
+
+    [Test]
+    public async Task GetNumber_FirstFiveHundred_ReturnsFirstFiveHundredPentagonalNumbers()
+    {
+        // ARRANGE
+        var path = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Assets\PentagonalNumbersTests_500.txt");
+
+        // ACT & ASSERT
+        var n = 1;
+        await foreach (var line in File.ReadLinesAsync(path))
+        {
+            Assert.That(long.TryParse(line, out var expectedPentagonal), Is.True);
+            Assert.That(PentagonalNumbers.GetNumber(n++), Is.EqualTo(expectedPentagonal));
+        }
+    }
+
+    [Test]
+    public async Task IsPentagonalNumber_PandigitalNumbers_ReturnsTrue()
+    {
+        // ARRANGE
+        var path = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Assets\PentagonalNumbersTests_500.txt");
+
+        // ACT & ASSERT
+        var n = 1;
+        await foreach (var line in File.ReadLinesAsync(path))
+        {
+            Assert.That(long.TryParse(line, out var pentagonal), Is.True);
+            Assert.That(PentagonalNumbers.IsPentagonalNumber(pentagonal), Is.True);
+        }
+    }
+}
---
+++ ProjectEuler.Mathematics/Numbers/PentagonalNumbers.cs
@@ -0,0 +1,47 @@
+﻿namespace ProjectEuler.Mathematics.Numbers;
+
+/// <summary>
+/// A pentagonal number is a figural number that extends the concept of triangular and square numbers to the pentagon,
+/// but, unlike the first two, the patterns involved in the construction of pentagonal numbers are not rotationally symmetrical.
+/// </summary>
+public static class PentagonalNumbers
+{
+    /// <summary>
+    /// Generates pentagonal numbers until a specified <paramref name="count"/> is met.
+    /// </summary>
+    /// <param name="count">The number of pentagonal numbers to generate.</param>
+    public static IEnumerable<long> Generate(long count)
+    {
+        for (var n = 1; n <= count; n++)
+        {
+            yield return GetNumber(n);
+        }
+    }
+
+    /// <summary>
+    /// Gets the <paramref name="n"/>th pentagonal number.
+    /// </summary>
+    /// <param name="n">The position of the pentagonal number.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="n"/> is lower than 1.</exception>
+    public static long GetNumber(long n)
+    {
+        if (n <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(n));
+        }
+
+        return n * (3 * n - 1) / 2;
+    }
+
+    /// <summary>
+    /// Determines whether the specified <paramref name="number"/> is a pentagonal number.
+    /// </summary>
+    /// <param name="number">The number to check.</param>
+    /// <returns><see langword="true"/> if the number is a pentagonal number; <see langword="false"/> otherwise.</returns>
+    public static bool IsPentagonalNumber(long number)
+    {
+        var n = (1.0 + Math.Sqrt(1.0 + 24.0 * number)) / 6.0;
+
+        return double.IsPositive(n) && double.IsInteger(n);
+    }
+}
