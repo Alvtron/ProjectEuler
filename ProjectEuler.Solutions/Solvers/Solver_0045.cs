36af413aeb4c103f1c27ce79aab8155649d6fbf6
Thomas Angeland <thomas.angeland@gmail.com>
2023-03-22 00:02:03 +0100

    Added solver for problem 45.

---
+++ ProjectEuler.Solutions/Solvers/Solver_0045.cs
@@ -0,0 +1,25 @@
+﻿using ProjectEuler.Mathematics.Numbers;
+using ProjectEuler.Solutions.Answers;
+
+namespace ProjectEuler.Solutions.Solvers;
+
+public class Solver_0045 : ISolver
+{
+    public async Task<Answer> SolveAsync(CancellationToken cancellationToken = default)
+    {
+        return await Task.FromResult(FindNextTriangleNumber(286));
+    }
+
+    private static Answer FindNextTriangleNumber(long position)
+    {
+        foreach (var triangular in TriangularNumbers.Between(position, long.MaxValue))
+        {
+            if (PentagonalNumbers.IsPentagonalNumber(triangular) && HexagonalNumbers.IsHexagonalNumber(triangular))
+            {
+                return triangular;
+            }
+        }
+
+        throw new InvalidOperationException("Unable to find the next triangle number");
+    }
+}
