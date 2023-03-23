801e311006ea5f5677d13af53a8dd3a6df68e702
Thomas Angeland <thomas.angeland@gmail.com>
2023-03-23 22:38:13 +0100

    Added solver for problem 46.

---
+++ ProjectEuler.Solutions/Solvers/Solver_0046.cs
@@ -0,0 +1,41 @@
+﻿using ProjectEuler.Mathematics.Numbers;
+using ProjectEuler.Solutions.Answers;
+
+namespace ProjectEuler.Solutions.Solvers;
+
+public class Solver_0046 : ISolver
+{
+    public async Task<Answer> SolveAsync(CancellationToken cancellationToken = default)
+    {
+        var smallestComposite = FindSmallestCompositeRefutingGoldbach();
+        return await Task.FromResult(smallestComposite);
+    }
+
+    private static Answer FindSmallestCompositeRefutingGoldbach()
+    {
+        foreach (var composite in CompositeNumbers.Generate().Where(long.IsOddInteger))
+        {
+            var any = false;
+
+            foreach (var prime in PrimeNumbers.Generate().Where(long.IsOddInteger))
+            {
+                if (prime > composite - 2L)
+                {
+                    break;
+                }
+
+                if (SquareNumbers.IsSquare((composite - prime) / 2L))
+                {
+                    any = true;
+                }
+            }
+
+            if (!any)
+            {
+                return composite;
+            }
+        }
+
+        throw new InvalidOperationException("Unable to disprove Goldbach's.");
+    }
+}
