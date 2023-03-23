ed5e5911fcc9b581b23a4f4d7a1be8fdffaf16b9
Thomas Angeland <thomas.angeland@gmail.com>
2023-03-23 22:35:27 +0100

    Added SquareNumbers helper class.

---
+++ ProjectEuler.Mathematics.Tests/Numbers/SquareNumbersTests.cs
@@ -0,0 +1,72 @@
+﻿using ProjectEuler.Mathematics.Numbers;
+
+namespace ProjectEuler.Mathematics.Tests.Numbers;
+
+[TestFixture]
+public class SquareNumbersTests
+{
+    private string[]? actualSquares;
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetUpAsync()
+    {
+        var path = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Assets\SquareNumbersTests_1000000.txt");
+        this.actualSquares = await File.ReadAllLinesAsync(path);
+    }
+
+    [Test]
+    public void IsSquare_1000000Squares_ReturnsTrue()
+    {
+        foreach (var line in this.actualSquares!)
+        {
+            // ARRANGE
+            Assert.That(long.TryParse(line, out var squareNumber), Is.True);
+
+            // ACT
+            var isSquare = SquareNumbers.IsSquare(squareNumber);
+
+            // ASSERT
+            Assert.That(isSquare, Is.True,
+                $"{squareNumber} is not a perfect square.");
+        }
+    }
+
+    [Test]
+    public void Generate_1000000_AllAreSquares()
+    {
+        // ACT
+        var generatedSquares = SquareNumbers.Generate().Take(1_000_000);
+
+        // ASSERT
+        foreach (var (generated, actual) in generatedSquares.Zip(this.actualSquares!))
+        {
+            Assert.That(generated.ToString(), Is.EqualTo(actual));
+        }
+    }
+
+    [Test]
+    public void Between_0And1000000_AllAreSquares()
+    {
+        // ACT
+        var generatedSquares = SquareNumbers.Between(0, 1_000_000);
+
+        // ASSERT
+        foreach (var (generated, actual) in generatedSquares.Zip(this.actualSquares!))
+        {
+            Assert.That(generated.ToString(), Is.EqualTo(actual));
+        }
+    }
+
+    [Test]
+    public void Between_1000000And0_AllAreSquares()
+    {
+        // ACT
+        var generatedSquares = SquareNumbers.Between(1_000_000, 0);
+
+        // ASSERT
+        foreach (var (generated, actual) in generatedSquares.Zip(this.actualSquares!.Reverse()))
+        {
+            Assert.That(generated.ToString(), Is.EqualTo(actual));
+        }
+    }
+}
---
+++ ProjectEuler.Mathematics/Numbers/SquareNumbers.cs
@@ -0,0 +1,69 @@
+﻿namespace ProjectEuler.Mathematics.Numbers;
+
+/// <summary>
+/// A square number is a positive integer that can be formed by multiplying two smaller positive integers.
+/// Equivalently, it is a positive integer that has at least one divisor other than 1 and itself.
+/// </summary>
+public static class SquareNumbers
+{
+    /// <summary>
+    /// Generates all perfect squares in ascending order.
+    /// </summary>
+    /// <remarks>Enumeration should be stopped using linq.</remarks>
+    public static IEnumerable<long> Generate()
+    {
+        for (var number = 0L;; number++)
+        {
+            yield return number * number;
+        }
+    }
+
+    /// <summary>
+    /// Find all perfect square between a <paramref name="start"/> and <paramref name="end"/>.
+    /// </summary>
+    /// <remarks>
+    /// If <paramref name="start"/> is greater than <paramref name="end"/>,
+    /// the square values are generated in descending order.
+    /// </remarks>
+    /// <param name="start">The start number that defines one point in the search space.</param>
+    /// <param name="end">The end number that defines another point in the search space.</param>
+    public static IEnumerable<long> Between(long start, long end)
+    {
+        start = Math.Max(start, 0L);
+        end = Math.Max(end, 0L);
+        var step = start < end ? 1L : -1L;
+
+        for (var number = start; number <= end; number += step)
+        {
+            if (!IsSquare(number))
+            {
+                continue;
+            }
+
+            yield return number;
+        }
+    }
+
+    /// <summary>
+    /// Determines whether the specified <paramref name="number"/> is considered a perfect square.
+    /// </summary>
+    /// <param name="number">The number to check.</param>
+    /// <returns><see langword="true"/> if the number is a perfect square; <see langword="false"/> otherwise.</returns>
+    public static bool IsSquare(long number)
+    {
+        if (long.IsNegative(number))
+        {
+            return false;
+        }
+
+        var root = Math.Sqrt(number);
+        if (!double.IsInteger(root))
+        {
+            return false;
+        }
+
+        var longRoot = (long)root;
+        
+        return longRoot * longRoot == number;
+    }
+}
