bbe50f207fa083bfed91ac3850472b7cc8e0724d
Thomas Angeland <thomas.angeland@gmail.com>
2023-03-26 03:05:42 +0200

    Added enumerable count extensions.

---
+++ ProjectEuler.Extensions.Tests/Collections/EnumerableCountExtensions.cs
@@ -0,0 +1,207 @@
+﻿using ProjectEuler.Extensions.Collections;
+
+namespace ProjectEuler.Extensions.Tests.Collections;
+
+[TestFixture]
+public class EnumerableCountExtensions
+{
+    [Test]
+    public void CountEquals_CountIsNumberOfElements_ReturnsTrue()
+    {
+        // ARRANGE
+        var zero = Enumerable.Range(1, 0);
+        var one = Enumerable.Range(1, 1);
+        var ten = Enumerable.Range(1, 10);
+        var hundred = Enumerable.Range(1, 100);
+        var thousand = Enumerable.Range(1, 1000);
+        var million = Enumerable.Range(1, 1_000_000);
+        var billion = Enumerable.Range(1, 1_000_000_000);
+
+        // ACT & ASSERT
+        Assert.That(zero.CountEquals(0), Is.True);
+        Assert.That(one.CountEquals(1), Is.True);
+        Assert.That(ten.CountEquals(10), Is.True);
+        Assert.That(hundred.CountEquals(100), Is.True);
+        Assert.That(thousand.CountEquals(1000), Is.True);
+        Assert.That(million.CountEquals(1_000_000), Is.True);
+        Assert.That(billion.CountEquals(1_000_000_000), Is.True);
+    }
+
+    [Test]
+    public void CountEquals_CountIsLowerThanNumberOfElements_ReturnsFalse()
+    {
+        // ARRANGE
+        var zero = Enumerable.Range(1, 0);
+        var one = Enumerable.Range(1, 1);
+        var ten = Enumerable.Range(1, 10);
+        var hundred = Enumerable.Range(1, 100);
+        var thousand = Enumerable.Range(1, 1000);
+        var million = Enumerable.Range(1, 1_000_000);
+        var billion = Enumerable.Range(1, 1_000_000_000);
+
+        // ACT & ASSERT
+        Assert.That(zero.CountEquals(0 - 1), Is.False);
+        Assert.That(one.CountEquals(1 - 1), Is.False);
+        Assert.That(ten.CountEquals(10 - 1), Is.False);
+        Assert.That(hundred.CountEquals(100 - 1), Is.False);
+        Assert.That(thousand.CountEquals(1000 - 1), Is.False);
+        Assert.That(million.CountEquals(1_000_000 - 1), Is.False);
+        Assert.That(billion.CountEquals(1_000_000_000 - 1), Is.False);
+    }
+
+    [Test]
+    public void CountEquals_CountIsHigherThanNumberOfElements_ReturnsFalse()
+    {
+        // ARRANGE
+        var zero = Enumerable.Range(1, 0);
+        var one = Enumerable.Range(1, 1);
+        var ten = Enumerable.Range(1, 10);
+        var hundred = Enumerable.Range(1, 100);
+        var thousand = Enumerable.Range(1, 1000);
+        var million = Enumerable.Range(1, 1_000_000);
+        var billion = Enumerable.Range(1, 1_000_000_000);
+
+        // ACT & ASSERT
+        Assert.That(zero.CountEquals(0 + 1), Is.False);
+        Assert.That(one.CountEquals(1 + 1), Is.False);
+        Assert.That(ten.CountEquals(10 + 1), Is.False);
+        Assert.That(hundred.CountEquals(100 + 1), Is.False);
+        Assert.That(thousand.CountEquals(1000 + 1), Is.False);
+        Assert.That(million.CountEquals(1_000_000 + 1), Is.False);
+        Assert.That(billion.CountEquals(1_000_000_000 + 1), Is.False);
+    }
+
+    [Test]
+    public void CountLowerThan_CountIsNumberOfElements_ReturnsFalse()
+    {
+        // ARRANGE
+        var zero = Enumerable.Range(1, 0);
+        var one = Enumerable.Range(1, 1);
+        var ten = Enumerable.Range(1, 10);
+        var hundred = Enumerable.Range(1, 100);
+        var thousand = Enumerable.Range(1, 1000);
+        var million = Enumerable.Range(1, 1_000_000);
+        var billion = Enumerable.Range(1, 1_000_000_000);
+
+        // ACT & ASSERT
+        Assert.That(zero.CountLowerThan(0), Is.False);
+        Assert.That(one.CountLowerThan(1), Is.False);
+        Assert.That(ten.CountLowerThan(10), Is.False);
+        Assert.That(hundred.CountLowerThan(100), Is.False);
+        Assert.That(thousand.CountLowerThan(1000), Is.False);
+        Assert.That(million.CountLowerThan(1_000_000), Is.False);
+        Assert.That(billion.CountLowerThan(1_000_000_000), Is.False);
+    }
+
+    [Test]
+    public void CountLowerThan_CountIsLowerThanNumberOfElements_ReturnsFalse()
+    {
+        // ARRANGE
+        var zero = Enumerable.Range(1, 0);
+        var one = Enumerable.Range(1, 1);
+        var ten = Enumerable.Range(1, 10);
+        var hundred = Enumerable.Range(1, 100);
+        var thousand = Enumerable.Range(1, 1000);
+        var million = Enumerable.Range(1, 1_000_000);
+        var billion = Enumerable.Range(1, 1_000_000_000);
+
+        // ACT & ASSERT
+        Assert.That(zero.CountLowerThan(0 - 1), Is.False);
+        Assert.That(one.CountLowerThan(1 - 1), Is.False);
+        Assert.That(ten.CountLowerThan(10 - 1), Is.False);
+        Assert.That(hundred.CountLowerThan(100 - 1), Is.False);
+        Assert.That(thousand.CountLowerThan(1000 - 1), Is.False);
+        Assert.That(million.CountLowerThan(1_000_000 - 1), Is.False);
+        Assert.That(billion.CountLowerThan(1_000_000_000 - 1), Is.False);
+    }
+
+    [Test]
+    public void CountLowerThan_CountIsHigherThanNumberOfElements_ReturnsTrue()
+    {
+        // ARRANGE
+        var zero = Enumerable.Range(1, 0);
+        var one = Enumerable.Range(1, 1);
+        var ten = Enumerable.Range(1, 10);
+        var hundred = Enumerable.Range(1, 100);
+        var thousand = Enumerable.Range(1, 1000);
+        var million = Enumerable.Range(1, 1_000_000);
+        var billion = Enumerable.Range(1, 1_000_000_000);
+
+        // ACT & ASSERT
+        Assert.That(zero.CountLowerThan(0 + 1), Is.True);
+        Assert.That(one.CountLowerThan(1 + 1), Is.True);
+        Assert.That(ten.CountLowerThan(10 + 1), Is.True);
+        Assert.That(hundred.CountLowerThan(100 + 1), Is.True);
+        Assert.That(thousand.CountLowerThan(1000 + 1), Is.True);
+        Assert.That(million.CountLowerThan(1_000_000 + 1), Is.True);
+        Assert.That(billion.CountLowerThan(1_000_000_000 + 1), Is.True);
+    }
+
+    // asd asd asd
+
+    [Test]
+    public void CountHigherThan_CountIsNumberOfElements_ReturnsFalse()
+    {
+        // ARRANGE
+        var zero = Enumerable.Range(1, 0);
+        var one = Enumerable.Range(1, 1);
+        var ten = Enumerable.Range(1, 10);
+        var hundred = Enumerable.Range(1, 100);
+        var thousand = Enumerable.Range(1, 1000);
+        var million = Enumerable.Range(1, 1_000_000);
+        var billion = Enumerable.Range(1, 1_000_000_000);
+
+        // ACT & ASSERT
+        Assert.That(zero.CountHigherThan(0), Is.False);
+        Assert.That(one.CountHigherThan(1), Is.False);
+        Assert.That(ten.CountHigherThan(10), Is.False);
+        Assert.That(hundred.CountHigherThan(100), Is.False);
+        Assert.That(thousand.CountHigherThan(1000), Is.False);
+        Assert.That(million.CountHigherThan(1_000_000), Is.False);
+        Assert.That(billion.CountHigherThan(1_000_000_000), Is.False);
+    }
+
+    [Test]
+    public void CountHigherThan_CountIsLowerThanNumberOfElements_ReturnsTrue()
+    {
+        // ARRANGE
+        var zero = Enumerable.Range(1, 0);
+        var one = Enumerable.Range(1, 1);
+        var ten = Enumerable.Range(1, 10);
+        var hundred = Enumerable.Range(1, 100);
+        var thousand = Enumerable.Range(1, 1000);
+        var million = Enumerable.Range(1, 1_000_000);
+        var billion = Enumerable.Range(1, 1_000_000_000);
+
+        // ACT & ASSERT
+        Assert.That(zero.CountHigherThan(0 - 1), Is.True);
+        Assert.That(one.CountHigherThan(1 - 1), Is.True);
+        Assert.That(ten.CountHigherThan(10 - 1), Is.True);
+        Assert.That(hundred.CountHigherThan(100 - 1), Is.True);
+        Assert.That(thousand.CountHigherThan(1000 - 1), Is.True);
+        Assert.That(million.CountHigherThan(1_000_000 - 1), Is.True);
+        Assert.That(billion.CountHigherThan(1_000_000_000 - 1), Is.True);
+    }
+
+    [Test]
+    public void CountHigherThan_CountIsHigherThanNumberOfElements_ReturnsFalse()
+    {
+        // ARRANGE
+        var zero = Enumerable.Range(1, 0);
+        var one = Enumerable.Range(1, 1);
+        var ten = Enumerable.Range(1, 10);
+        var hundred = Enumerable.Range(1, 100);
+        var thousand = Enumerable.Range(1, 1000);
+        var million = Enumerable.Range(1, 1_000_000);
+        var billion = Enumerable.Range(1, 1_000_000_000);
+
+        // ACT & ASSERT
+        Assert.That(zero.CountHigherThan(0 + 1), Is.False);
+        Assert.That(one.CountHigherThan(1 + 1), Is.False);
+        Assert.That(ten.CountHigherThan(10 + 1), Is.False);
+        Assert.That(hundred.CountHigherThan(100 + 1), Is.False);
+        Assert.That(thousand.CountHigherThan(1000 + 1), Is.False);
+        Assert.That(million.CountHigherThan(1_000_000 + 1), Is.False);
+        Assert.That(billion.CountHigherThan(1_000_000_000 + 1), Is.False);
+    }
+}
---
+++ ProjectEuler.Extensions/Collections/EnumerableCountExtensions.cs
@@ -0,0 +1,106 @@
+﻿namespace ProjectEuler.Extensions.Collections;
+
+public static class EnumerableCountExtensions
+{
+    /// <summary>
+    /// Check if the number of elements in the source is equal to the specified count.
+    /// </summary>
+    /// <remarks>
+    /// This will never exhaust the source past the necessary number of elements.
+    /// This is in almost all circumstances faster than .Count() == x, where x is an arbitrary <see cref="int"/>.<br/>
+    /// If the source is a sequence that implements <see cref="ICollection{TSource}"/>, the Count-property is used instead.
+    /// </remarks>
+    /// <typeparam name="TSource">The element type in the source.</typeparam>
+    /// <param name="source">The source of elements.</param>
+    /// <param name="count">The number of elements that is checked.</param>
+    /// <returns>True if the number of elements in the source is identical to the specified count; false otherwise.</returns>
+    public static bool CountEquals<TSource>(this IEnumerable<TSource> source, int count)
+    {
+        if (source is null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        if (count < 0)
+        {
+            return false;
+        }
+
+        if (source is ICollection<TSource> collection)
+        {
+            return collection.Count == count;
+        }
+
+        if (count == 0)
+        {
+            return !source.Any();
+        }
+
+        using var enumerator = source.Skip(count - 1).GetEnumerator();
+        return enumerator.MoveNext() && !enumerator.MoveNext();
+    }
+
+    /// <summary>
+    /// Check if the number of elements in the source is higher than the specified count.
+    /// </summary>
+    /// <remarks>
+    /// This will never exhaust the source past the necessary number of elements.
+    /// This is in almost all circumstances faster than .Count() &gt; x, where x is an arbitrary <see cref="int"/>.<br/>
+    /// If the source is a sequence that implements <see cref="ICollection{TSource}"/>, the Count-property is used instead.
+    /// </remarks>
+    /// <typeparam name="TSource">The element type in the source.</typeparam>
+    /// <param name="source">The source of elements.</param>
+    /// <param name="count">The number of elements that is checked.</param>
+    /// <returns>True if the number of elements in the source is higher than the specified count; false otherwise.</returns>
+    public static bool CountHigherThan<TSource>(this IEnumerable<TSource> source, int count)
+    {
+        if (source is null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        if (count < 0)
+        {
+            return true;
+        }
+
+        if (source is ICollection<TSource> collection)
+        {
+            return collection.Count > count;
+        }
+
+        return source.Skip(count).Any();
+    }
+
+    /// <summary>
+    /// Check if the number of elements in the source is lower than the specified count.
+    /// </summary>
+    /// <remarks>
+    /// This will never exhaust the source past the necessary number of elements.
+    /// This is in almost all circumstances faster than .Count() &lt; x, where x is an arbitrary <see cref="int"/>.<br/>
+    /// If the source is a sequence that implements <see cref="ICollection{TSource}"/>, the Count-property is used instead.
+    /// </remarks>
+    /// <typeparam name="TSource">The element type in the source.</typeparam>
+    /// <param name="source">The source of elements.</param>
+    /// <param name="count">The number of elements that is checked.</param>
+    /// <returns>True if the number of elements in the source is lower than the specified count; false otherwise.</returns>
+    public static bool CountLowerThan<TSource>(this IEnumerable<TSource> source, int count)
+    {
+        if (source is null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        if (count < 1)
+        {
+            return false;
+        }
+
+        if (source is ICollection<TSource> collection)
+        {
+            return collection.Count < count;
+        }
+
+        return !source.Skip(count - 1).Any();
+    }
+}
