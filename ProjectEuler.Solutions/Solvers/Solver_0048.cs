421aad17221ed6851a21042c678668c5a8036780
Thomas Angeland <thomas.angeland@gmail.com>
2023-08-06 21:57:17 +0200

    Added solver for problem 48.

---
+++ ProjectEuler.Solutions/Solvers/Solver_0048.cs
@@ -0,0 +1,21 @@
+﻿using ProjectEuler.Solutions.Answers;
+using System.Numerics;
+
+namespace ProjectEuler.Solutions.Solvers;
+
+public class Solver_0048 : ISolver
+{
+    public async Task<Answer> SolveAsync(CancellationToken cancellationToken = default)
+    {
+        var sum = BigInteger.Zero;
+
+        for (var number = 1; number <= 1000; number++)
+        {
+            sum += BigInteger.Pow(number, number);
+        }
+
+        var lastDigits = string.Concat(sum.ToString().TakeLast(10));
+
+        return await Task.FromResult(lastDigits);
+    }
+}
