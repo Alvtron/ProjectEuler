08e1a65b2d9619fa8dbce105fd6d3057b83e9cd5
Thomas Angeland <thomas.angeland@gmail.com>
2024-08-17 20:08:06 +0200

    Added solver for problem 52.

---
+++ ProjectEuler.Solutions/Solvers/Solver_0052.cs
@@ -0,0 +1,38 @@
+﻿using ProjectEuler.Extensions.Numbers;
+using ProjectEuler.Solutions.Answers;
+
+namespace ProjectEuler.Solutions.Solvers;
+
+internal sealed class Solver_0052 : ISolver
+{
+    public Task<Answer> SolveAsync(CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult<Answer>(FindSmallestNumberWithPermutedMultiples(6));
+    }
+
+    private static long FindSmallestNumberWithPermutedMultiples(int targetNumberOfMultiples)
+    {
+        for (var number = 1;; number++)
+        {
+            var digits = number.Digits().ToHashSet();
+            var numberOfMultiples = 1;
+
+            for (var multiple = 2; multiple <= targetNumberOfMultiples; multiple++)
+            {
+                var product = number * multiple;
+
+                if (!digits.SetEquals(product.Digits()))
+                {
+                    break;
+                }
+
+                numberOfMultiples++;
+            }
+
+            if (numberOfMultiples == targetNumberOfMultiples)
+            {
+                return number;
+            }
+        }
+    }
+}
