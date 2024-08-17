0fe5a4619001d8ef792a1dd09fbd62399ac4e0a6
Thomas Angeland <thomas.angeland@gmail.com>
2024-08-17 21:46:38 +0200

    Added solver for problem 53.

---
+++ ProjectEuler.Mathematics/Combinatorics/Factorial.cs
@@ -0,0 +1,55 @@
+﻿using System.Numerics;
+
+namespace ProjectEuler.Mathematics.Combinatorics
+{
+    public static class Factorial
+    {
+        /// <summary>
+        /// Returns the factorial of the specified <paramref name="n"/>.
+        /// </summary>
+        /// <param name="n">The number to calculate the factorial of.</param>
+        public static BigInteger Of(int n)
+        {
+            var result = BigInteger.One;
+
+            for (var i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the factorial of the specified <paramref name="n"/>.
+        /// </summary>
+        /// <param name="n">The number to calculate the factorial of.</param>
+        public static BigInteger Of(long n)
+        {
+            var result = BigInteger.One;
+
+            for (var i = 2L; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the factorial of the specified <paramref name="n"/>.
+        /// </summary>
+        /// <param name="n">The number to calculate the factorial of.</param>
+        public static BigInteger Of(BigInteger n)
+        {
+            var result = BigInteger.One;
+
+            for (var i = new BigInteger(2); i <= n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProjectEuler.Solutions/Solvers/Solver_0053.cs
@@ -0,0 +1,26 @@
+﻿using ProjectEuler.Mathematics.Combinatorics;
+using ProjectEuler.Solutions.Answers;
+using System.Numerics;
+
+namespace ProjectEuler.Solutions.Solvers;
+
+internal sealed class Solver_0053 : ISolver
+{
+    public Task<Answer> SolveAsync(CancellationToken cancellationToken = default)
+    {
+        var result = FindCombinatoricSelectionsBetween(1, 100).Count(n => n > 1_000_000);
+
+        return Task.FromResult<Answer>(result);
+    }
+
+    private static IEnumerable<BigInteger> FindCombinatoricSelectionsBetween(int start, int end)
+    {
+        for (var n = start; n <= end; n++)
+        {
+            for (var r = 1; r <= n; r++)
+            {
+                yield return Factorial.Of(n) / (Factorial.Of(r) * Factorial.Of(n - r));
+            }
+        }
+    }
+}
