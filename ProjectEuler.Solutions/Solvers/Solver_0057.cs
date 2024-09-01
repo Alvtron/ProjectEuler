808b97dc2e5e51b93499bc16bf9bbcf6105f6738
Thomas Angeland <thomas.angeland@gmail.com>
2024-09-01 22:09:48 +0200

    Added solver for problem 57.

---
+++ ProjectEuler.Solutions/Solvers/Solver_0057.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+using ProjectEuler.Extensions.Numbers;
+using ProjectEuler.Solutions.Answers;
+
+namespace ProjectEuler.Solutions.Solvers;
+
+internal sealed class Solver_0057 : ISolver
+{
+    public Task<Answer> SolveAsync(CancellationToken cancellationToken = default)
+    {
+        var count = FindSquareRootConvergents(1000);
+        return Task.FromResult<Answer>(count);
+    }
+
+    private static int FindSquareRootConvergents(int expansions)
+    {
+        var count = 0;
+        var numerator = new BigInteger(3);
+        var denominator = new BigInteger(2);
+
+        for (var i = 1; i < expansions; i++)
+        {
+            numerator += 2 * denominator;
+            denominator = numerator - denominator;
+
+            if (numerator.Digits().Count() > denominator.Digits().Count())
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
