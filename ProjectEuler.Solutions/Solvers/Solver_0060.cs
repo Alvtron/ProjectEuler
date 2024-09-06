06f23a41bc3e118d2d9e9f9061253677c1d97f98
Thomas Angeland <thomas.angeland@gmail.com>
2024-09-06 22:28:29 +0200

    Added solver for problem 60.

---
+++ ProjectEuler.Solutions/Solvers/Solver_0060.cs
@@ -0,0 +1,61 @@
+﻿using ProjectEuler.Mathematics.Numbers;
+using ProjectEuler.Solutions.Answers;
+
+namespace ProjectEuler.Solutions.Solvers;
+
+internal sealed class Solver_0060 : ISolver
+{
+    public Task<Answer> SolveAsync(CancellationToken cancellationToken = default)
+    {
+        var primeSet = FindPrimeSet(5);
+        return Task.FromResult<Answer>(primeSet.Sum());
+    }
+
+    private readonly record struct Pair(long A, long B);
+
+    private static HashSet<long> FindPrimeSet(int setSize)
+    {
+        var primeSets = new List<HashSet<long>> { new() };
+        var cache = new Dictionary<Pair, bool>();
+
+        foreach (var nextPrime in PrimeNumbers.Generate())
+        {
+            var newSets = new List<HashSet<long>>();
+            foreach (var set in primeSets)
+            {
+                if (!set.All(pairedPrime => AreConcatenationsPrime(new Pair(pairedPrime, nextPrime))))
+                {
+                    continue;
+                }
+
+                var newSet = new HashSet<long>(set) { nextPrime };
+                if (newSet.Count == setSize)
+                {
+                    return newSet;
+                }
+
+                newSets.Add(newSet);
+            }
+
+            primeSets.AddRange(newSets);
+        }
+
+        throw new InvalidOperationException("No prime set found");
+
+        bool AreConcatenationsPrime(Pair pair)
+        {
+            if (cache.TryGetValue(pair, out var result))
+            {
+                return result;
+            }
+
+            var isPrimePair = IsConcatenationPrime(pair.A, pair.B) && IsConcatenationPrime(pair.B, pair.A);
+            return cache[pair] = isPrimePair;
+        }
+
+        bool IsConcatenationPrime(long a, long b)
+        {
+            return PrimeNumbers.IsPrime(CombinedNumbers.Combine(a, b));
+        }
+    }
+}
