5927eec25baa4ed7572df7c7374c9693a8822346
Thomas Angeland <thomas.angeland@gmail.com>
2024-10-01 20:21:42 +0200

    Added solver for problem 62.

---
+++ ProjectEuler.Solutions/Solvers/Solver_0062.cs
@@ -0,0 +1,40 @@
+﻿using ProjectEuler.Extensions.Numbers;
+using ProjectEuler.Solutions.Answers;
+
+namespace ProjectEuler.Solutions.Solvers;
+
+internal sealed class Solver_0062 : ISolver
+{
+    public Task<Answer> SolveAsync(CancellationToken cancellationToken = default)
+    {
+        var cubePermutation = FindCubePermutation();
+        return Task.FromResult<Answer>(cubePermutation);
+    }
+
+    private static long FindCubePermutation()
+    {
+        var cubePermutation = 0L;
+        var cubeMap = new Dictionary<string, List<long>>();
+
+        for (var n = 1L; cubePermutation == 0; n++)
+        {
+            var cube = n * n * n;
+            var key = string.Concat(cube.Digits().OrderBy(d => d));
+
+            if (!cubeMap.ContainsKey(key))
+            {
+                cubeMap[key] = [];
+            }
+
+            cubeMap[key].Add(cube);
+
+            if (cubeMap[key].Count == 5)
+            {
+                cubePermutation = cubeMap[key].First();
+            }
+        }
+
+        return cubePermutation;
+    }
+
+}
