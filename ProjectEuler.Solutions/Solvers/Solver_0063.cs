5c4761626fb621b3230d8689ea2bc0124de0ef2d
Thomas Angeland <thomas.angeland@gmail.com>
2024-10-01 21:01:39 +0200

    Added solver for problem 63.

---
+++ ProjectEuler.Solutions/Solvers/Solver_0063.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+using ProjectEuler.Solutions.Answers;
+
+namespace ProjectEuler.Solutions.Solvers;
+
+internal sealed class Solver_0063 : ISolver
+{
+    public Task<Answer> SolveAsync(CancellationToken cancellationToken = default)
+    {
+        var count = CountNthPowerDigits();
+        return Task.FromResult<Answer>(count);
+    }
+
+    private static int CountNthPowerDigits()
+    {
+        var count = 0;
+
+        for (var number = 1; number < 10; number++)
+        {
+            for (var exponent = 1; ; exponent++)
+            {
+                var result = BigInteger.Pow(number, exponent);
+                var length = result.ToString().Length;
+                if (length == exponent)
+                {
+                    count++;
+                }
+                else if (length < exponent)
+                {
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }   
+}
