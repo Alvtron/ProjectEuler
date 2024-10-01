5196a0a7439e53d50e6fcf5ebda1402037a605d6
Thomas Angeland <thomas.angeland@gmail.com>
2024-10-01 21:11:51 +0200

    Added solver for problem 65.

---
+++ ProjectEuler.Solutions/Solvers/Solver_0065.cs
@@ -0,0 +1,42 @@
+﻿using ProjectEuler.Solutions.Answers;
+using System.Numerics;
+using ProjectEuler.Extensions.Numbers;
+
+namespace ProjectEuler.Solutions.Solvers;
+
+internal sealed class Solver_0065 : ISolver
+{
+    public Task<Answer> SolveAsync(CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult<Answer>(FindSumOfDigits());
+    }
+
+    private static long FindSumOfDigits()
+    {
+        var numerator = BigInteger.One;
+        var denominator = BigInteger.Zero;
+
+        for (var n = 100; n > 0; n--)
+        {
+            BigInteger term;
+            if (n == 1)
+            {
+                term = 2;
+            }
+            else if (n % 3 == 0)
+            {
+                term = 2 * (n / 3);
+            }
+            else
+            {
+                term = 1;
+            }
+
+            (numerator, denominator) = (denominator, numerator);
+
+            numerator += term * denominator;
+        }
+
+        return numerator.Digits().Sum();
+    }
+}
