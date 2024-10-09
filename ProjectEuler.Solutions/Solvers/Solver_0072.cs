07fb94242f7f9399a373c4ca02cfbb92ad518840
Thomas Angeland <thomas.angeland@gmail.com>
2024-10-09 23:15:56 +0200

    Added solver for problem 72.

---
+++ ProjectEuler.Solutions/Solvers/Solver_0072.cs
@@ -0,0 +1,13 @@
+﻿using ProjectEuler.Mathematics.Numbers;
+using ProjectEuler.Solutions.Answers;
+
+namespace ProjectEuler.Solutions.Solvers;
+
+internal sealed class Solver_0072 : ISolver
+{
+    public Task<Answer> SolveAsync(CancellationToken cancellationToken = default)
+    {
+        var phi = EulerTotientNumbers.Generate(1_000_000);
+        return Task.FromResult<Answer>(phi.Sum() - 1);
+    }
+}
