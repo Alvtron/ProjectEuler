430d1628861932e7ad144f184dbc53e39472d07f
Thomas Angeland <thomas.angeland@gmail.com>
2024-11-17 19:53:00 +0100

    Added solver for problem 76.

---
+++ ProjectEuler.Solutions/Solvers/Solver_0076.cs
@@ -0,0 +1,27 @@
+﻿using ProjectEuler.Solutions.Answers;
+
+namespace ProjectEuler.Solutions.Solvers;
+
+internal sealed class Solver_0076 : ISolver
+{
+    public Task<Answer> SolveAsync(CancellationToken cancellationToken = default)
+    {
+        var result = CountWaysToSum(target: 100);
+        return Task.FromResult<Answer>(result);
+    }
+
+    private static long CountWaysToSum(int target)
+    {
+        var ways = new long[target + 1];
+        ways[0] = 1;
+        for (var i = 1; i < target; i++)
+        {
+            for (var j = i; j <= target; j++)
+            {
+                ways[j] += ways[j - i];
+            }
+        }
+
+        return ways[target];
+    }
+}
